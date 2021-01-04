127a218491939ebc2900e7c47b4a0e33dcfe065d
sofka <ageevasophia@gmail.com>
2021-01-04 23:30:53 +0700

    Add first Dom Elements

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('Hello world');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const el = (
+  <div>
+    <h1>ToDoItem</h1>
+    <input placeholder="search"/>
+    <ul>
+      <li>First Item</li>
+      <li>Second Item</li>
+    </ul>
+  </div>
+);
+
+ReactDOM.render(el, document.getElementById('root'));
