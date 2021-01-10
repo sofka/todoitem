b8b2f30e6e90343dd269acbdc22dae2b59661a52
sofka <ageevasophia@gmail.com>
2021-01-10 23:50:43 +0700

    CHange function to class. Add first Listener and work with set state

--- src/components/ToDoListItem.js
+++ src/components/ToDoListItem.js
@@ -1,21 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../style/todo-list-item.css';
 
-const ToDoListItem = ({ label, important = false }) => {
-    const style = {
-        color: important ? 'tomato' : 'black'
-    };
-    return (
-        <span className="todo-list-item">
-            <span className="todo-list-item-label" style={style}>{label}</span>
-            <button type="button" className="btn btn-outline-danger btn-sm float-right">
-                <i className="fa fa-exclamation" />
-            </button>
-            <button type="button" className="btn btn-outline-success btn-sm float-right">
-                <i className="fa fa-trash-o" />
-            </button>
-        </span>
-    );
-};
 
-export default ToDoListItem;
+export default class ToDoListItem extends Component {
+
+    state = {
+        done: false,
+        important: false
+    };
+
+    onLabelClick = () => {
+        this.setState({
+            done: true
+        });
+    }
+
+    render() {
+        const { label } = this.props;
+        const { done, important } = this.state;
+        const style = {
+            color: important ? 'tomato' : 'black',
+            fontWeight: important ? 'bold' : 'normal'
+        };
+
+        let classNames = 'todo-list-item';
+        if (done) {
+            classNames += ' done';
+        }
+
+        if (important) {
+            classNames += ' important';
+        }
+        return (
+            <span className={classNames}>
+                <span className="todo-list-item-label"
+                    style={style}
+                    onClick={this.onLabelClick}>
+                    {label}
+                </span>
+                <button type="button" className="btn btn-outline-danger btn-sm float-right">
+                    <i className="fa fa-exclamation" />
+                </button>
+                <button type="button" className="btn btn-outline-success btn-sm float-right">
+                    <i className="fa fa-trash-o" />
+                </button>
+            </span>
+        );
+    };
+}
