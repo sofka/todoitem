168825e197bdb30f6be23b1259a2d5aaa66357e9
sofka <ageevasophia@gmail.com>
2021-01-24 23:32:40 +0700

    Инпут в форме добавления пункта сделала контролируемым элементом.

--- src/components/ItemAddForm.js
+++ src/components/ItemAddForm.js
@@ -11,17 +11,22 @@ export default class ItemAddForm extends Component {
             label: e.target.value
         });
     }
-    onSubmit = (e)=>{
+    onSubmit = (e) => {
         e.preventDefault();
         this.props.addNewItem(this.state.label);
+        this.setState({
+            label: ''
+        })
     }
     render() {
         return (
             <form className="item-add-form d-flex"
-            onSubmit={this.onSubmit}>
+                onSubmit={this.onSubmit}>
+                    {/* Контролируемый элемент - в него передается значение через state */}
                 <input type="text" className="form-control"
                     onChange={this.onLabelChange}
-                    placeholder="Что ещё нужно сделать" />
+                    placeholder="Что ещё нужно сделать"
+                    value={this.state.label} />
                 <button type="submit"
                     className="btn btn-primary">
                     Добавить
